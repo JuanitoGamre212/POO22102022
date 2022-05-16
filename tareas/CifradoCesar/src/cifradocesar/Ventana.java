913f284daef7e51b3c712daedaf57b1536e55838
JuanitoGamer <memiliano246@gmail.com>
2022-05-16 12:54:54 -0500

    tarea

--- tareas/CifradoCesar/src/cifradocesar/Ventana.java
+++ tareas/CifradoCesar/src/cifradocesar/Ventana.java
@@ -1,75 +1,93 @@
 
 package cifradocesar;
 
-import java.awt.Color;
-import java.util.Scanner;
+
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
 public class Ventana extends JFrame {
+
+    private final JLabel resultado;
+    private final JLabel etiqueta2;
+    private final JLabel etiqueta;
+    private final JButton boton;
+    private final JTextField codificador;
+    private final JTextField cuadroTexto;
     
-    public Ventana(){
-    this.setSize(500, 300);// tamaño de la ventana
-        setLocation(500, 250);
-    this.setTitle("CIFRADO CESAR");
-    
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public Ventana() throws HeadlessException {
+     this.setTitle("CIFRADO CESAR");
+     this.setSize(500, 300);//tamaño ventana
+        setLocation(500, 250);// posicion inicial
+     this.setVisible(true);
+        FlowLayout layout = new FlowLayout();
+     this.setLayout(layout);
+        cuadroTexto = new JTextField(20);
+        codificador =new JTextField(20);
+        boton =new JButton("Codificar");
+        etiqueta =new JLabel("Ingrese el texo a codirficar");
+        etiqueta2 =new JLabel("ingrese el despasamiento");
+        resultado =new JLabel(" El texto codificado es: ");
         
-    this.getContentPane().setBackground(Color.GREEN);//establecemos color}
-    componentes ();
+        
+        this.getContentPane().add(etiqueta);
+        this.getContentPane().add(cuadroTexto);    
+        this.getContentPane().add(etiqueta2);    
+        this.getContentPane().add(codificador);
+        this.getContentPane().add(boton);
+        this.getContentPane().add(resultado);
+        //this.pack();
+        //this.setVisible(true);
+        
+       
+        
+        this.addWindowListener(new WindowAdapter() {
+         @Override
+         public void windowClosing(WindowEvent e) {   
+            System.exit(0);
+          }
+        
+        });
+        
+   this.boton.addMouseListener(new MouseAdapter() {
+         @Override
+         public void mouseClicked(MouseEvent e) {
+             String min= "abcdefghijklmnñopqrstuvwxyz";
+             String fraseC;//string ddonde se almacenara la frase cifrada
+             fraseC = "";
+             String frase;//string que almacenen el texto de el usuario
+             frase =cuadroTexto.getText();
+             int desplazamiento = Integer.parseInt(codificador.getText());
+             
+             for (int i=0;i<frase.length(); i++){
+                for (int j=0;j<min.length(); j++){
+                    if(frase.charAt(i)==min.charAt(j)){
+                        if(j+desplazamiento >=min.length()){
+                        }
+                        
+                        else{
+                        
+                            fraseC +=min.charAt(j+desplazamiento);
+                        }
+                    }
+                 
+                }
+                  
+             }
+             
+             resultado.setText("Mensaje codificado es: "+ fraseC); //aqui muesto el texto codificado
+         }
+       
+   });
+        
     }
-    
-    private void componentes (){
-        
-        
-        JPanel panel=new JPanel(); // creacion de un panel
-    this.getContentPane().add(panel);
-    panel.setBackground(Color.YELLOW);
-   
-    
-        JLabel etiqueta=new JLabel("Bienvenido al mejor Traductor de codigo Cesar");       
-        panel.add(etiqueta);
-        
-        JLabel vacio= new JLabel("                                     ");
-        panel.add(vacio);
-        
-        JLabel etiqueta3 =new JLabel("Ingrese palabra");
-        panel.add(etiqueta3);
-        
-        
-        JTextField texto=new JTextField(15);
-        panel.add(texto);
-
-           JLabel vacio2= new JLabel("                                     ");
-        panel.add(vacio2);
-        
-        JLabel clave =new JLabel("Cantidad de Desplasamiento");
-        panel.add(clave);
-        
-        JTextField cl=new JTextField(15);
-        panel.add(cl);
-        
-
-        JButton boton = new JButton("Codificar");
-        JTextField resultado=texto;
-        panel.add(boton);
-
-        
-        JLabel etiqueta2=new JLabel("Palabra codificada");
-        panel.add(etiqueta2);
-                
-        JTextField texto2= new JTextField(20);
-        panel.add(texto2);
-        
-        
-                
-    }
-    
-   
-    
-    
+     
 }
